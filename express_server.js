9e2ec114aa4fafb2b8eef188b60a71a3cc705ad7
james-truong <james.truong@hotmail.com>
2019-10-29 00:10:31 +0000

    added new POST method as a new GET method for shortlinks

--- express_server.js
+++ express_server.js
@@ -25,6 +25,7 @@ const urlDatabase = {
 function generateRandomString() {
   let result = '';
   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
   for ( let i = 0; i < 6; i++ ) {
      result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
@@ -41,12 +42,21 @@ app.get("/urls/new", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let random = generateRandomString();
+  urlDatabase[random] = req.body.longURL;
+  res.redirect("/urls/");
+  console.log(urlDatabase);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  // const longURL = ...
+  let short = req.params.shortURL;
+  res.redirect(urlDatabase[short]);
 });
 
 app.listen(PORT, () => {
