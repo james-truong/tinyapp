3c0c743bab71c5f73d4b7e5ace28442222cfdd90
james-truong <james.truong@hotmail.com>
2019-10-29 00:53:03 +0000

    added a delete function

--- express_server.js
+++ express_server.js
@@ -59,6 +59,11 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(urlDatabase[short]);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+res.redirect("/urls"); 
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
